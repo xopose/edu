78a1255f85de0e44f06a458c212e8aabd737939b
xopose <lazarevds2001@gmail.com>
2022-12-28 19:39:48 +0300

    ok

--- src/main/java/org/lazer/Main.java
+++ src/main/java/org/lazer/Main.java
@@ -1,7 +1,10 @@
 package org.lazer;
 
+import org.lazer.box.Box;
+
 public class Main {
     public static void main(String[] args) {
-
+        Box box1 = new Box(100, 20, 42.5);
+        System.out.println(box1.depth * box1.width * box1.height);
     }
 }
---
+++ src/main/java/org/lazer/box/Box.java
@@ -0,0 +1,14 @@
+package org.lazer.box;
+
+public class Box {
+    public double width;
+    public double height;
+    public double depth;
+
+    public Box(double width, double height, double depth){
+        this.width=width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+}
