3132600f6ffce253849bb84d6eeca3a9a9fa9607
xopose <lazarevds2001@gmail.com>
2023-01-20 18:06:51 +0300

    13\FileStream

--- src/main/java/org/lazer/Main.java
+++ src/main/java/org/lazer/Main.java
@@ -7,14 +7,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, System.console().charset()));
-        System.out.println("Ввод");
-        String c;
-        do{
-            c = br.readLine();
-            System.out.println(c);
+        int i;
+        try(var fin = new FileInputStream("src/main/java/org/lazer/files/input");
+            var fout = new FileOutputStream("src/main/java/org/lazer/files/output")) {
+            do{
+                i = fin.read();
+                if (i != -1) fout.write(i);
+            }while (i != -1);
+        }catch (IOException e){
+            System.out.println(e);
         }
-        while (!c.equals("aaaa"));
     }
 }
