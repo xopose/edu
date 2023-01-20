4b38c3cf9ccaa617856aa241b30dc6ed1aa04387
xopose <lazarevds2001@gmail.com>
2023-01-20 20:16:30 +0300

    14\<>

--- src/main/java/org/lazer/Main.java
+++ src/main/java/org/lazer/Main.java
@@ -4,18 +4,25 @@ import org.lazer.resources.*;
 
 import java.io.*;
 
-
+class Gen<T extends Number>{
+    T ob;
+    Gen(T o){
+        ob = o;
+    }
+    T getOb(){
+        return ob;
+    }
+    void showType(){
+        System.out.println("ob является типом" + ob.getClass().getName());
+    }
+}
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int i;
-        try(var fin = new FileInputStream("src/main/java/org/lazer/files/input");
-            var fout = new FileOutputStream("src/main/java/org/lazer/files/output")) {
-            do{
-                i = fin.read();
-                if (i != -1) fout.write(i);
-            }while (i != -1);
-        }catch (IOException e){
-            System.out.println(e);
-        }
+    public static void main(String[] args){
+        Gen<Integer> iOb;
+        iOb = new Gen<Integer>(88);
+        iOb.showType();
+//        Gen<String> sOb;
+//        sOb = new Gen<>("sdadadadadgszb");
+//        sOb.showType();
     }
 }
