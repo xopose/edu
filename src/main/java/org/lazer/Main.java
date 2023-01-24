92599a694ba7df56db859a347f9b5ed422fbd7cb
xopose <lazarevds2001@gmail.com>
2023-01-24 15:38:23 +0300

    15\LambdaParameters

--- src/main/java/org/lazer/Main.java
+++ src/main/java/org/lazer/Main.java
@@ -2,17 +2,16 @@ package org.lazer;
 
 import org.lazer.resources.*;
 
-interface MyNumber<T extends Number>{
-    T getValue();
+interface NumericTest{
+    boolean test(int n, int d);
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        MyNumber myNum;
-        myNum = () -> 123.45;
-        System.out.println(myNum.getValue());
-        myNum = () -> 123L;
-        System.out.println(myNum.getValue());
+        NumericTest isEven = (n, d) -> (n%d)==0;
+        if (isEven.test(10, 2)) System.out.println("2 является множителем 10");
+        if (isEven.test(9,2)) System.out.println("2 не является множителем 9");
+        if (isEven.test(9,3)) System.out.println("3 является множителем 9");
     }
 }
