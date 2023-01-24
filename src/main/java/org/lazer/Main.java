93a92ef8582a644bf9948d0b71f5622e1f997135
xopose <lazarevds2001@gmail.com>
2023-01-24 15:20:50 +0300

    15\Lambda

--- src/main/java/org/lazer/Main.java
+++ src/main/java/org/lazer/Main.java
@@ -2,35 +2,17 @@ package org.lazer;
 
 import org.lazer.resources.*;
 
-import static org.lazer.GetMethDemo.isIn;
-
-class GetMethDemo<T extends Comparable<T>> implements UniInterface<T>{
-    private double arg;
-    <T extends Number> GetMethDemo(T arg){
-        this.arg = arg.doubleValue();
-    }
-    static <T extends Comparable<T>, V extends T> boolean isIn(T x, V[] y){
-        for (int i=0; i<y.length; i++){
-            if (y[i] == x) return true;
-
-        }
-        return false;
-    }
-
-    @Override
-    public T min() {
-        return null;
-    }
-
-    @Override
-    public T max() {
-        return null;
-    }
+interface MyNumber<T extends Number>{
+    T getValue();
 }
+
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(isIn(5, arr));
+        MyNumber myNum;
+        myNum = () -> 123.45;
+        System.out.println(myNum.getValue());
+        myNum = () -> 123L;
+        System.out.println(myNum.getValue());
     }
 }
