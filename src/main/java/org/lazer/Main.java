02520a4870686c13df4428f3c12e807fb96ce406
xopose <lazarevds2001@gmail.com>
2023-01-24 19:57:31 +0300

    15\ok!

--- src/main/java/org/lazer/Main.java
+++ src/main/java/org/lazer/Main.java
@@ -4,59 +4,32 @@ import org.jetbrains.annotations.NotNull;
 import org.lazer.resources.*;
 
 interface MyFunc<T>{
-    boolean func(T v1, T v2);
+    int func(T[] vals, T v);
 }
 
-class HighTemp{
-    private int hTemp;
-    HighTemp(int hTemp){
-        this.hTemp = hTemp;
-    }
-
-    boolean sameTemp(HighTemp ht2){
-        return hTemp == ht2.hTemp;
-    }
-
-    boolean lessThenTemp(HighTemp ht2){
-        return hTemp < ht2.hTemp;
-    }
+class MyArrayOps{
+    static <T> int countMathing(T[] vals, T v){
+        int count = 0;
+        for (int i = 0; i<vals.length; i++){
+            if(vals[i] == v)
+                count++;
+        }
+        return count;
+    };
 }
 
 public class Main {
-    /** массив, ссылка на MyFunc, искомое значение
-     */
-    static <T> int counter(T vals[], MyFunc<T> f, T v){
-        int count = 0;
-        for (int i = 0; i<vals.length; i++)
-            if (f.func(vals[i], v))
-                count++;
-
-        return count;
+    static <T> int myOp(MyFunc<T> f, T vals[], T v){
+        return f.func(vals, v);
     };
     public static void main(String[] args){
+        Integer[] vals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 4, 5, 3, 5, 9, 8, 7, 0, 0, 0, 0, 4, 4, 4};
+        String[] str = {"one", "two", "three", "two"};
         int count;
-
-        HighTemp[] weekDayHigh = {
-                new HighTemp(89), new HighTemp(82),
-                new HighTemp(90), new HighTemp(89),
-                new HighTemp(89), new HighTemp(91),
-                new HighTemp(84), new HighTemp(83)
-        };
-        count = counter(weekDayHigh, HighTemp::sameTemp, new HighTemp(89));
+        count = myOp(MyArrayOps::<Integer>countMathing, vals, 4);
         System.out.println(count);
 
-        HighTemp[] weekDayHigh2 = {
-                new HighTemp(34), new HighTemp(12),
-                new HighTemp(24), new HighTemp(19),
-                new HighTemp(18), new HighTemp(12),
-                new HighTemp(-1), new HighTemp(13)
-        };
-        count = counter(weekDayHigh2, HighTemp::sameTemp, new HighTemp(12));
+        count = myOp(MyArrayOps::<String>countMathing, str, "two");
         System.out.println(count);
-        count = counter(weekDayHigh, HighTemp::lessThenTemp, new HighTemp(89));
-        System.out.println(count);
-        count = counter(weekDayHigh2, HighTemp::lessThenTemp, new HighTemp(19));
-        System.out.println(count);
-
     }
 }
